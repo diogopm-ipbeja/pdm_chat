2153e9c6ce6eee7ed7465d02036e3bd9a733495b
Diogo Pina Manique <diogoprpm@gmail.com>
2020-11-10 17:43:37 +0000

    initial commit

---
+++ app/src/main/java/pt/ipbeja/pdm/chat/MainActivity.java
@@ -0,0 +1,21 @@
+package pt.ipbeja.pdm.chat;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
